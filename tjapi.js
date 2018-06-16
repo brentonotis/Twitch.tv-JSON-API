abb81a13b1a9fe2e5831a7c3871d44f8a5fb58e9
Brenton Otis <36424273+brentonotis@users.noreply.github.com>
2018-06-16 10:30:36 -0500

    Update tjapi.js
    update api to glitch

--- tjapi.js
+++ tjapi.js
@@ -1,6 +1,6 @@
 let streamers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
-let urlStreams = "https://wind-bow.gomix.me/twitch-api/streams/";
-let urlUsers = "https://wind-bow.gomix.me/twitch-api/users/";
+let urlStreams = "https://wind-bow.glitch.me/twitch-api/streams/";
+let urlUsers = "https://wind-bow.glitch.me/twitch-api/users/";
 let onlineWindow = document.getElementById('online');
 let offlineWindow = document.getElementById('offline');
 let onlineBtn = document.getElementById('onBtn');
